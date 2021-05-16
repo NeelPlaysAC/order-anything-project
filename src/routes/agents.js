b001fcc43aa69096218881fff960d71bd398eba2
Neel <42354574+NeelPlaysAC@users.noreply.github.com>
2021-05-16 08:41:55 +0530

    Update agents.js

--- src/routes/agents.js
+++ src/routes/agents.js
@@ -17,7 +17,7 @@ router.get("/me", auth, async (req, res) => {
   res.send(agent);
 });
 
-// Agent route to change assigned order's status
+// Sign Up route for new Agents
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
